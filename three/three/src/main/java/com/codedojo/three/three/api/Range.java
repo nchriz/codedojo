75ad250421b2a8a6977b9d993769b0d765e708cf
Christofer Malmberg <christofer.malmberg@travelex.com>
2019-11-20 16:47:26 +0000

    Update Code Dojo Three

--- three/three/src/main/java/com/codedojo/three/three/api/Range.java
+++ three/three/src/main/java/com/codedojo/three/three/api/Range.java
@@ -1,5 +1,7 @@
 package com.codedojo.three.three.api;
 
+import com.codedojo.three.three.service.RangeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Range {
 
+    @Autowired
+    private RangeService rangeService;
+
     @RequestMapping(method = RequestMethod.GET, value = "/range/{range}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getRange(@PathVariable(value = "range") String range) {
+        rangeService.findRange(Integer.valueOf(range));
         return ResponseEntity.of(null);
     }
 
---
+++ three/three/src/main/java/com/codedojo/three/three/service/RangeService.java
@@ -0,0 +1,13 @@
+package com.codedojo.three.three.service;
+
+import com.codedojo.three.three.model.RangeEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RangeService {
+
+    public RangeEntity findRange(int range) {
+        return null;
+    }
+
+}
